abeba51e6f759f97b7da526a9eda185c07b46414
tizianocarranza <tizianocarranza0910@gmail.com>
2024-07-05 10:54:07 -0300

    Colors mods

--- src/componentes/elements/footer/Footer.jsx
+++ src/componentes/elements/footer/Footer.jsx
@@ -39,7 +39,7 @@ function Footer() {
 
 
         <div className="footer__disclaimer">
-          <p>©2024 Emperia. All Rights Reserved.</p>
+          <p>©2024 Tiziano. All Rights Reserved.</p>
         </div>
       </div>
     </section>
